c466152f666a3ec34dc43fdb606a2dee9fc02988
juliop110 <juliootero7@gmail.com>
2021-09-22 11:48:45 -0300

    documentação Swagger2 em porgresso

--- src/main/java/br/com/otero/parkinglot/ParkinglotApplication.java
+++ src/main/java/br/com/otero/parkinglot/ParkinglotApplication.java
@@ -2,12 +2,14 @@ package br.com.otero.parkinglot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class ParkinglotApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ParkinglotApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ParkinglotApplication.class, args);
+    }
 
 }
